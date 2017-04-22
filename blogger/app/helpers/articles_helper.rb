5b604c2b319c569e90203f8f91123dd23244a8e6
Husam Nasar <husam.nasar@gmail.com>
2017-04-22 13:02:26 +0100

    added a few gems

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
 
   def article_params
-    return params.require(:article).permit(:title, :body, :tag_list);
+    return params.require(:article).permit(:title, :body, :tag_list, :image);
   end
 end
--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -3,6 +3,19 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+  #Basically, telling Rails that the Article Model has the ability to have attachments....
+  #And, validating the type of attachments against 3 types of image files "jpg, jpeg, and png"...
+  #Paperclip gem method...
+
+  #Developer Note:
+  #As of version 4.0, all attachments are required to include a content_type validation, a file_name validation,
+  #or to explicitly state that they’re not going to have either.
+  #Paperclip raises MissingRequiredValidatorError error if you do not do this.
+  #So, we add the validates_attachment_content_type line so that our model will validate that it is receiving a proper filetype.
+
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   #Returns a string which contains all the tags separated with a ", " between them.
   def tag_list
     self.tags.collect{ |tag|
