75742a845408422ac0d93bb506bf0323660ffd1b
Husam Nasar <husam.nasar@gmail.com>
2017-04-23 23:10:54 +0100

    Sorcery authentication complete

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
+  resources :authors
   resources :articles do
     resources :comments
   end
 
   resources :tags
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
